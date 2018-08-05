6aa0d7b9f3356662fd6e7988764b5e232214e975
surjit <surjit.kumar@gmail.com>
2018-08-05 06:17:43 +0530

    Changes for Typescript compatibility

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Hello from './src/component/Hello'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,9 +22,10 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Nativefg!</Text>
+        <Text style={styles.welcome}>Welcome to React Nativeessss!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Hello name={'surjit'} enthusiasmLevel={1} />
       </View>
     );
   }
